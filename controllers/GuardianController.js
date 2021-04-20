7f73b5cb6e411bf93dfb81d9cba641deca9278e3
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-20 21:49:03 +0100

    finished controller files

--- controllers/GuardianController.js
+++ controllers/GuardianController.js
@@ -1,11 +1,20 @@
 const axios = require("axios");
 
 module.exports = {
-    getBooksNews: function(req,res) {
-        const {query} = req;
+  getBooksNews: function (req, res) {
+    const { query } = req;
 
-        axios.get("https://content.guardianapis.com/search?q=books&api-key=ea5c2603-5999-43b3-8ddf-e3db33f0592d", {
-            params: query
-        }).then(response => console.log(response))
-    }
-}
+    axios
+      .get(
+        "https://content.guardianapis.com/search?q=books&api-key=ea5c2603-5999-43b3-8ddf-e3db33f0592d",
+        {
+          params: query,
+        }
+      )
+      .then((response) => {
+        console.log(response);
+        res.json(response.data);
+      })
+      .catch((err) => res.status(422).json(err));
+  },
+};
--- controllers/OpenLibraryController.js
+++ controllers/OpenLibraryController.js
@@ -1,11 +1,17 @@
 const axios = require("axios");
 
 module.exports = {
-    getOpenLibraryBooks: function(req,res) {
-        const {query} = req;
+  getOpenLibraryBooks: function (req, res) {
+    const { query } = req;
 
-        axios.get("http://openlibrary.org/search.json?q=", {
-            params: query
-        }).then(response => console.log(response))
-    }
-}
+    axios
+      .get("http://openlibrary.org/search.json?q=", {
+        params: query,
+      })
+      .then((response) => {
+        console.log(response);
+        res.json(response.data);
+      })
+      .catch((err) => res.status(422).json(err));
+  },
+};
