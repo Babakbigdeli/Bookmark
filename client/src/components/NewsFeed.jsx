228970dd93d3dcffb92b02c7c6b0959ebe630fcb
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-29 19:10:44 +0100

    added functionality to news feed to render news articles from guardian api

--- client/src/components/NewsFeed.jsx
+++ client/src/components/NewsFeed.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Feed, Icon } from 'semantic-ui-react';
 
-function NewsFeed() {
-  return <div className="newsFeed"></div>;
+
+export default class NewsFeed extends React.Component{
+state = {
+  loading: true,
+  news: null
+};
+
+async componentDidMount () {
+       const url = "https://content.guardianapis.com/search?q=books&api-key=ea5c2603-5999-43b3-8ddf-e3db33f0592d";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      this.setState({ news: data.response.results.splice(5,10), loading: false });
 }
 
-export default NewsFeed;
+
+
+
+render(){
+  return (
+  <Feed>
+  <Feed.Event>
+    <Feed.Label>
+      <Icon name='newspaper' />
+    </Feed.Label>
+    <Feed.Content>
+      <Feed.Summary>
+        {this.state.loading || !this.state.news ? (
+          <div>loading...</div>
+        ) : (
+        this.state.news.map((newsArticle) => {
+          return (
+            <div>
+              <div>{newsArticle.webTitle}</div>
+                <a>{newsArticle.webUrl}</a>
+            </div>
+          )
+        })
+        )}
+      </Feed.Summary>
+    </Feed.Content>
+  </Feed.Event>
+</Feed>
+  )}
+
+  }
