86c57603111db72af343e2cef7ef7754ec38fd5d
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-29 13:00:01 +0100

    banner compo brought in

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-function Banner() {
-  return <div className="banner"></div>;
-}
+const Banner = () => {
+  return (
+    <div className="banner">
+      <p className="movingtext">
+        <span>
+          If you love books, you'll love Bookmark. Here you can search books,
+          save them into categories and keep track of your progress when reading
+          them.
+        </span>
+      </p>
+    </div>
+  );
+};
 
 export default Banner;
