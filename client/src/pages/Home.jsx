9426887e62a9a22f88a43bc15c647ff4128bfd0e
omayma ahmad <omayma.ahmad25@gmail.com>
2021-04-29 19:11:49 +0100

    format document

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,6 @@ import { Grid } from "semantic-ui-react";
 import { useAuth0 } from "@auth0/auth0-react";
 import API from "../utils/API";
 
-
 function Home() {
   const [subKey, setSubKey] = useState("");
   const [books, setBooks] = useState([]);
@@ -56,70 +55,77 @@ function Home() {
     });
   };
 
-    return (
-     <>
-     <Navbar />
-     <Grid divided="vertically">
-       <Grid.Row columns={2}>
-         <Grid.Column>
-           <Searchbar 
-           q={q}
-           handleChange={handleChange}
-           handleSubmit={handleSubmit}
-           />
-           <ul>
-        {books.length ? (
-          books.map((book) => (
-           <SearchResultCard 
-           id={book.id}
-              title={book.volumeInfo.title}
-              authors={book.volumeInfo.authors.join(", ")}
-              description={book.volumeInfo.description}
-              number_of_pages={book.volumeInfo.pageCount}
-              language={book.volumeInfo.language}
-              PastButton={() => (
-                <button
-                  value="past"
-                  onClick={(event) => saveBookToDB(event.target.value, book.id)}
-                >
-                  Save to History
-                </button>
+  return (
+    <>
+      <Navbar />
+      <Grid divided="vertically">
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Searchbar
+              q={q}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+            />
+            <ul>
+              {books.length ? (
+                books.map((book) => (
+                  <SearchResultCard
+                    id={book.id}
+                    title={book.volumeInfo.title}
+                    authors={book.volumeInfo.authors.join(", ")}
+                    description={book.volumeInfo.description}
+                    number_of_pages={book.volumeInfo.pageCount}
+                    language={book.volumeInfo.language}
+                    PastButton={() => (
+                      <button
+                        value="past"
+                        onClick={(event) =>
+                          saveBookToDB(event.target.value, book.id)
+                        }
+                      >
+                        Save to History
+                      </button>
+                    )}
+                    PresentButton={() => (
+                      <button
+                        value="present"
+                        onClick={(event) =>
+                          saveBookToDB(event.target.value, book.id)
+                        }
+                      >
+                        Save to Currently Reading
+                      </button>
+                    )}
+                    FutureButton={() => (
+                      <button
+                        value="future"
+                        onClick={(event) =>
+                          saveBookToDB(event.target.value, book.id)
+                        }
+                      >
+                        Save to Want to Read
+                      </button>
+                    )}
+                  />
+                ))
+              ) : (
+                <p>No books to display. Are you sure you have searched?</p>
               )}
-              PresentButton={() => (
-                <button
-                  value="present"
-                  onClick={(event) => saveBookToDB(event.target.value, book.id)}
-                >
-                  Save to Currently Reading
-                </button>
-              )}
-              FutureButton={() => (
-                <button
-                  value="future"
-                  onClick={(event) => saveBookToDB(event.target.value, book.id)}
-                >
-                  Save to Want to Read
-                </button>
-              )}
-              />
-              ))
-        ) : (
-          <p>No books to display. Are you sure you have searched?</p>
-        )}
-        </ul>
-         </Grid.Column>
-         <Grid.Column>
-           <NewsFeed />
-         </Grid.Column>
-       </Grid.Row>
-       <Grid.Row columns={1}>
-         <Grid.Column>
-           <Banner />
-         </Grid.Column>
-       </Grid.Row>
-     </Grid>
-     <Footer/>
-   </>
-    )}
+            </ul>
+          </Grid.Column>
+          <Grid.Column>
+            <NewsFeed />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={1}>
+          <Grid.Column>
+            <Banner />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Footer />
+    </>
+  );
+}
 
 export default Home;
