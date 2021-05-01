8dfcca17ee3c496c45ba0c4184fb26622c704f37
omayma ahmad <omayma.ahmad25@gmail.com>
2021-05-01 09:02:31 +0100

    new search bar with styling

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import './style/Searchbar.css';
 
 function Searchbar({ q, handleChange, handleSubmit }) {
   return (
-    <div>
-      <input type="text" value={q} onChange={handleChange} />
-      <button onClick={handleSubmit} type="submit" className="submitbutton">
-        Search
-      </button>
-    </div>
+    <div id="cover">
+    <form method="get" action="">
+      <div class="tb">
+        <div class="td"><input type="text" placeholder="Search" required value={q} onChange={handleChange}></input></div>
+        <div class="td" id="s-cover">
+          <button onClick={handleSubmit} type="submit">
+            <div id="s-circle"></div>
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+  
   );
 }
+
 
 export default Searchbar;
