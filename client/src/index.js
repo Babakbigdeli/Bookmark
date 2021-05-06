10d2ab175a9fd916542396aa3e2021c26865b8bf
Babak Bigdeli <babakbigdeli@gmail.com>
2021-05-06 16:02:35 +0100

    Alert added to onclicks

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Auth0Provider} from '@auth0/auth0-react';
 import 'semantic-ui-css/semantic.min.css';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
 const  domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
+const options = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+}
+
 ReactDOM.render(
+  <AlertProvider template={AlertTemplate} {...options}>
   <Auth0Provider
   domain = {domain}
   clientId = {clientId}
   redirectUri = {window.location.origin}>
     <App />
-  </Auth0Provider>,
+  </Auth0Provider>
+  </AlertProvider>,
   document.getElementById('root')
 );
 
